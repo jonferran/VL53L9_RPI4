8186762de420340bb66be844be97c02ccb2b88a2
jonferran <jonferran@yahoo.com>
2026-07-15 02:17:36 -0700

    Update test_i2c_ranging.c

--- test_i2c_ranging.c
+++ test_i2c_ranging.c
@@ -1,3 +1,14 @@
+/* Copyright (c) 2026 Jon Ferran.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
